4215ccfec6d77da77319c362f91324b817c55112
Daniel Taboada <119310520+dtaboadaS@users.noreply.github.com>
2023-03-01 22:23:09 +0100

    Delete CRUD directory

--- CRUD/BBDD/constants.php
+++
@@ -1,7 +0,0 @@
-<?php	    
-        //Se definen valores de conexión (por si hubiera migración o cambio futuro)
-	    define("HOST", "localhost");
-	    define("USER", "root");
-	    define("PASSWORD", "");
-	    define("NOMBRE_BBDD", "universidad");
-?>
